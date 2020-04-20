4125961ea2f26d9ea0108b70605c9f37ab9951c3
Belim <github@mirinsoft.com>
2020-04-20 17:42:11 +0200

    Add files via upload

---
+++ scripts/basicPrivacyTemplate.ps1
@@ -0,0 +1,64 @@
+﻿########## Disable telemetry in Windows 10 ##########
+# As you use Windows 10, Microsoft will collect usage information. All its options are available in Settings -> Privacy - Feedback and Diagnostics. There you can set the options "Diagnostic and usage data" to Basic, Enhanced and Full.
+# This will set diagnostic data to Basic, which is the lowest level available for all consumer versions of Windows 10
+New-ItemProperty -Path "HKLM:\Software\Microsoft\Windows\CurrentVersion\Policies\DataCollection" -Name AllowTelemetry -Value 0 -Force
+# Stop and Disable Diagnostic Tracking Service
+Stop-Service -Name DiagTrack
+Set-Service -Name DiagTrack -StartupType Disabled
+# Stop and Disable dmwappushservice Service
+Stop-Service -Name dmwappushservice
+Set-Service -Name dmwappushservice -StartupType Disabled
+
+########## Block automatic Installation of apps ##########
+# Microsoft flushes various apps into the system without being asked, especially games such as Candy Crush Saga. Users have to uninstall these manually if they don't want them on their computer. 
+# To prevent these downloads from starting in the first place, a small intervention in the registry helps.
+New-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\ContentDeliveryManager" -Name SilentInstalledAppsEnabled -Value 0 -Force
+
+########## Disable web search in start menu ########## 
+# The search in Windows 10 returns local files, programs and settings but also web results by default. While that may be useful to some users of the operating system, it is irritating or useless for others. 
+# Since most web results are generic search suggestions, it is just a faster way of running searches instead of getting results directly displayed in the interface.
+New-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\Windows Search" -Name ConnectedSearchUseWeb -Value 0 -Force
+
+########## Disable Location tracking ##########
+# When Location Tracking is turned on, Windows and its apps are allowed to detect the current location of your computer or device. 
+# This can be used to pinpoint your exact location, e.g. Map traces the location of PC and helps you in exploring nearby restaurants.
+New-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\CapabilityAccessManager\ConsentStore\location" -Name Value -Value Deny -Force
+ 
+########## Turn off Advertising ID for Relevant Ads ##########
+# Windows 10 comes integrated with advertising. Microsoft assigns a unique identificator to track your activity in the Microsoft Store and on UWP apps to target you with relevant ads.
+# If someone is giving you personalized ads, it means they are tracking your data. Turn off the advertising feature from Windows 10 with this script to stay secure.
+New-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\AdvertisingInfo" -Name Enabled -Value 0 -Force
+New-ItemProperty -Path "HKLM\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\AdvertisingInfo" -Name Enabled -Value 0 -Force
+
+########## Turn off help Microsoft improve typing and writing ########## 
+# When the Getting to know you privacy setting is turned on for inking & typing personalization in Windows 10, you can use your typing history and handwriting patterns to create a local user dictionary for you that is used to make better typing suggestions and improve handwriting recognition for each of the languages you use.
+New-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\InputPersonalization" -Name AllowInputPersonalization -Value 0 -Force
+New-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\InputPersonalization" -Name RestrictImplicitInkCollection -Value 1 -Force
+New-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\InputPersonalization" -Name RestrictImplicitTextCollection -Value 1 -Force
+New-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\HandwritingErrorReports" -Name PreventHandwritingErrorReports -Value 1 -Force
+New-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\TabletPC" -Name PreventHandwritingDataSharing -Value 1 -Force
+
+########## Do not show feedback notifications ########## 
+New-ItemProperty -Path "HKCU:\Software\Microsoft\Siuf\Rules" -Name PeriodInNanoSeconds -Value 0 -Force
+New-ItemProperty -Path "HKCU:\Software\Microsoft\Siuf\Rules" -Name NumberOfSIUFInPeriod -Value 0 -Force
+
+########## Disable Clipboard history  ########## 
+# With Windows 10 build 17666 or later, Microsoft has allowed cloud synchronization of clipboard. It is a special feature to sync clipboard content across all your devices connected with your Microsoft Account.
+New-ItemProperty -Path "HKCU:\Software\Microsoft\Clipboards" -Name EnableClipboardHistory -Value 0 -Force
+
+########## Disable Timeline history  ########## 
+# Microsoft made Timeline available to the public with Windows 10 build 17063. It collects a history of activities you've performed, including files you've opened and web pages you've viewed in Edge.
+New-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\System" -Name EnableActivityFeed -Value 0 -Force
+
+########## Disable off Wi-Fi Sense ########## 
+# Wi-Fi Sense is a feature in Windows 10 that allows you to connect to your friends shared Wi-Fi connections. 
+# That is, you and your friends may opt to share your or their Wi-Fi connections. If your computer is logged into a Microsoft account, by default it will share your Wi-Fi password with your Skype, Outlook and Facebook friends, which means your Wi-Fi password will be sent to Microsoft.
+# You should at least stop your PC from sending your Wi-Fi password.
+New-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\WcmSvc\wifinetworkmanager\config" -Name AutoConnectAllowedOEM -Value 0 -Force
+
+########## Disable Customer Experience Improvement Program ########## 
+Get-ScheduledTask -TaskPath "\Microsoft\Windows\Customer Experience Improvement Program\" | Disable-ScheduledTask
+
+########## Prevent using diagnostic data ########## 
+#Starting with Windows 10 build 15019, a new privacy setting to "let Microsoft provide more tailored experiences with relevant tips and recommendations by using your diagnostic data" has been added.
+New-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Privacy" -Name TailoredExperiencesWithDiagnosticDataEnabled -Value 0 -Force
